7cb78612bc20a31f08269225f84a26a54f7a0b9e
SaiSumanthLokinindi <saisumanthlokinindi@gmail.com>
2022-10-20 03:28:18 +0530

    Added generate password functionality

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
+"use strict"
+
 const range = document.getElementById('passwordLength');
 const rangeTooltip = document.getElementById('rangeValue');
-range.value = 12;
 
 const uppercaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const lowercaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const numberCharacters = ["0","1","2","3","4","5","6","7","8","9"];
-const specialCharacters = ["\"","!","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","{","|","}","~"];
+const specialCharacters = ["\"","!","#","$","%","&","'","(",")","*","+","-",".","/",":",";","<","=",">","?","@","[","]","^","_","{","|","}","~",","];
+const uppercaseFilter = document.getElementById('uppercaseFilter');
+const lowercaseFilter = document.getElementById('lowercaseFilter');
+const numbersFilter = document.getElementById('numbersFilter');
+const symbolsFilter = document.getElementById('symbolsFilter');
+const password = document.getElementById('password');
+
+const getRandomInt = (length)=>{
+    return Math.floor(Math.random() * (length));
+}
+
+const getRandomCharacter = (charSet)=>{
+    return charSet[getRandomInt(charSet.length)];
+}
+
+const validateCharacter = (character, prevCharacter)=>{
+    if(character===prevCharacter) return true;
+    if(isNaN(parseInt(character)) || isNaN(parseInt(prevCharacter))) return false;
+    else{
+        return parseInt(prevCharacter) + 1 === parseInt(character);
+    }
+}
 
 const setTooltip = ()=>{
     const tooltipValue = Number(((range.value - range.min)*100)/(range.max-range.min));
@@ -14,24 +36,37 @@ const setTooltip = ()=>{
     rangeTooltip.style.left = `calc(${tooltipValue}% + ${tooltipPosition}px - 25px)`;
 }
 
-document.addEventListener("DOMContentLoaded", setTooltip);
+document.addEventListener("DOMContentLoaded", ()=>{
+    setTooltip();
+    generatePassword();
+});
 
 range.addEventListener("input",setTooltip);
 
 
 const generatePassword = ()=>{
-    const uppercaseFilter = document.getElementById('uppercaseFilter');
-    const lowercaseFilter = document.getElementById('lowercaseFilter');
-    const numbersFilter = document.getElementById('numbersFilter');
-    const symbolsFilter = document.getElementById('symbolsFilter');
-    const characterSet = [];
-    if(uppercaseFilter.checked) characterSet += uppercaseFilter;
-    if(lowercaseFilter.checked) characterSet += lowercaseFilter;
-    if(numbersFilter.checked) characterSet += numbersFilter;
-    if(symbolsFilter.checked) characterSet += symbolsFilter;
-
+    let characterSet = [];
+    const passwordText = [];
+    if(uppercaseFilter.checked) characterSet = characterSet.concat(uppercaseCharacters);
+    if(lowercaseFilter.checked) characterSet = characterSet.concat(lowercaseCharacters);
+    if(numbersFilter.checked) characterSet = characterSet.concat(numberCharacters);
+    if(symbolsFilter.checked) characterSet = characterSet.concat(specialCharacters);
+    let i=0;
+    if(characterSet.length){
+        while(i<parseInt(range.value)){
+            let char = getRandomCharacter(characterSet);
+            console.log(char);
+            if(i!==0){
+                while(validateCharacter(char,passwordText[i-1])){
+                    char = getRandomCharacter(characterSet);
+                }
+            }
+            passwordText.push(char);
+            i++;
+        }
+        console.log(passwordText);
+        password.innerText = passwordText.join('');
+        
+    }
+    else password.innerText = "";
 }
-
-const str = `"!#$%&'()*+,-./:;<=>?@[]^_{|}~`;
-
-console.log(str.split(""));
